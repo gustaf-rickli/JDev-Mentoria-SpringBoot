5a7e567a652094bdcc05b9d0b37bed03a3573933
Gustavo Rickli Agibert Aragão <gustavorickli256@gmail.com>
2024-10-31 02:31:29 -0300

    first commit projeto mentoria java jdev

---
+++ src/main/java/jdev/mentoria/lojavirtual/JDevMentoriaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package jdev.mentoria.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JDevMentoriaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JDevMentoriaSpringBootApplication.class, args);
+	}
+
+}
